5a5274f1149872aac326869dc819dd97fa1f21dc
DH <psycn@naver.com>
2025-08-15 00:29:23 +0900

    flatList 사용해서 게시글 추가

--- app/(tabs)/posts.tsx
+++ app/(tabs)/posts.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function Posts() {
   const posts: {id: number, title: string}[] = [
@@ -14,12 +14,45 @@ export default function Posts() {
     {id: 10, title: "열 번째 게시글"},
   ]
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {posts.map(post => (
-        <Text key={post.id} style={{ fontSize: 18, marginVertical: 10 }}>
-          {post.id}. {post.title} 
-        </Text>
-      ))}
+    <View style={styles.postsContainer}>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.listWrap}
+        renderItem={({ item }) => (
+          <Text style={styles.postItem}>
+            {item.id}. {item.title}
+          </Text>
+        )}
+      />
     </View>
   );
 }
+
+const WIDTH = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  postsContainer: {
+    alignContent: "center",
+  },
+  listWrap: {
+    width: WIDTH - 16,
+    paddingTop: 70, // 상단 여백 추가
+    paddingBottom: 16, // 하단 여백 추가
+    marginHorizontal: 8, // 좌우에 8px씩 여백 추가
+  },
+  postItem: {
+    backgroundColor: "white",
+    padding : 16,
+    borderRadius: 10,
+    marginBottom: 10,
+    height: 100,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84, 
+    elevation: 5, // 안드로이드에서 그림자 효과를 주기 위한 속성
+    fontSize: 16,
+
+  }
+})
